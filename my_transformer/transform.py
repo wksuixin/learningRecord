27ec62b07e84c6e606b8af4a018338a52357009c
wksuixin <1159734615@qq.com>
2024-11-18 14:18:47 +0800

    transform内容增加

--- my_transformer/transform.py
+++ my_transformer/transform.py
@@ -68,17 +68,70 @@ class MultiHeadAttention(nn.Module):
         self.output_linear = nn.Linear(embed_dim, embed_dim)
     
     def forward(self, query, key, value, query_mask=None, key_mask=None, mask=None):
-        x = torch.cat([h(query, key, value, query_mask, key_mask, mask) for h in self.heads], dim=-1)
+        x_list = [h(query, key, value, query_mask, key_mask, mask) for h in self.heads]
+        x = torch.cat(x_list, dim=-1)
         x = self.output_linear(x)
         return x
+    
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.linear_1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.linear_2 = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(0.3) #config.hidden_dropout_prob)
+        
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = self.gelu(x)
+        x = self.linear_2(x)
+        x = self.dropout(x)
+        return x
+    
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.layer_norm_1 = nn.LayerNorm(config.hidden_size)
+        self.layer_norm_2 = nn.LayerNorm(config.hidden_size)
+        self.attention = MultiHeadAttention(config)
+        self.feed_forward = FeedForward(config)
+    
+    def forward(self, x, mask=None):
+        hidden_state = self.layer_norm_1(x)
+        x = x+self.attention(hidden_state, hidden_state, hidden_state, mask=mask)
+        x = self.layer_norm_2(x)
+        x = x+self.feed_forward(x)
+        return x
+
+class MyEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.layer_norm = nn.LayerNorm(config.hidden_size, eps = 1e-12)
+        self.dropout = nn.Dropout()
+        
+    def forward(self, input_ids):
+        seq_length = input_ids.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long).unsqueeze(0)
+        token_embeddings = self.token_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings = token_embeddings + position_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings 
+    
+
 
 if __name__ == "__main__":
     model_path = "/home/wk/code/model/Qwen2.5-0.5B/"
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     config = AutoConfig.from_pretrained(model_path)
     token_emb = nn.Embedding(config.vocab_size, config.hidden_size)
+    feed_forward = FeedForward(config)
     
     texts = "这是一个测试transformer的程序"
+    inputs = tokenizer(texts, return_tensors="pt", add_special_tokens=False)
     inputs_embs = preprocess(tokenizer, token_emb, texts)
     Q  = inputs_embs
     texts = "这是我自己写的程序，是我学习transformer用到的程序"
@@ -93,6 +146,27 @@ if __name__ == "__main__":
     # print(outputs.shape)
     
     multi_head = MultiHeadAttention(config)
-    multi_head(Q, K, V)
+    head_output = multi_head(Q, K, V)
+    out = feed_forward(head_output)
+    print(out.shape)
+    
+    
+    encode_layer = TransformerEncoderLayer(config)
+    encode_layer(Q)
+    
+    embedding_layer = MyEmbeddings(config)
+    embedding_layer(inputs.input_ids)
+    
+    
+    import numpy as np
+    
+    a = np.random.randint(1, 100000, (2,10))
+    a = np.float32(a)
+    a = torch.from_numpy(a)
+    layer = nn.LayerNorm(10)
+    out = layer(a)
+    print(out)
+    
+    
     
     
